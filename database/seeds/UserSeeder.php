2df6cd2aef7a65d87904eff56fd3c73c1fdd970e
Rolando Arias <rolando@skywebplus.com>
2017-05-09 00:22:02 -0600

    update seed user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 use App\Models\Role;
 use App\Models\User;
 
@@ -8,46 +9,51 @@ class UserSeeder extends Seeder{
 
     public function run(){
         DB::table('users')->delete();
-
         $adminRole = Role::whereName('administrator')->first();
-        $userCliente = Role::whereName('cliente')->first();
-
-        $user = User::create(array(
+        $user1 = User::create(array(
             'name'    => 'John',
             'last_name'     => 'Doe',
             'email'         => 'john@admin.com',
-            'picture'         => '/assets/images/avatar.png',
             'password'      => Hash::make('admin123'),
-            'token'         => str_random(64),
-            'activated'     => true,
-            'tipo_user'     => 'email'
-        ));
-        $user->assignRole($adminRole);
-
-        $user = User::create(array(
-            'name'    => 'Jane',
-            'last_name'     => 'Doe',
-            'email'         => 'user1@front.com',
-            'password'      => Hash::make('u123'),
             'picture'         => '/assets/images/avatar.png',            
             'token'         => str_random(64),
             'activated'     => true,
             'tipo_user'     => 'email'
 
         ));
-        $user->assignRole($userCliente);
+        $user1->assignRole($adminRole);
+        /* */
+        factory(App\Models\User::class,50)
+        ->create()
+        ->each(function ($u) {
+            $userCliente = Role::whereName('cliente')->first();
+            $u->assignRole($userCliente);
+        });
 
-        $user1 = User::create(array(
-            'name'    => 'Jane',
-            'last_name'     => 'Doe',
-            'email'         => 'user2@front.com',
-            'password'      => Hash::make('u123'),
-            'picture'         => '/assets/images/avatar.png',            
-            'token'         => str_random(64),
-            'activated'     => true,
-            'tipo_user'     => 'facebook'
+        factory(App\Models\User::class,10)
+        ->create()
+        ->each(function ($u) {
+            $userCliente = Role::whereName('ventas')->first();
+            $u->assignRole($userCliente);
+        });
 
-        ));
-        $user1->assignRole($userCliente);
+        factory(App\Models\User::class,10)
+        ->create()
+        ->each(function ($u) {
+            $userCliente = Role::whereName('compras')->first();
+            $u->assignRole($userCliente);
+        });
+        factory(App\Models\User::class,10)
+        ->create()
+        ->each(function ($u) {
+            $userCliente = Role::whereName('staff')->first();
+            $u->assignRole($userCliente);
+        });
+        factory(App\Models\User::class,10)
+        ->create()
+        ->each(function ($u) {
+            $userCliente = Role::whereName('servicio')->first();
+            $u->assignRole($userCliente);
+        });
     }
 }
